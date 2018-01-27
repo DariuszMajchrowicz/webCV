42b313a56f498e9b3be946aebbb42655b30ea78e
DESKTOP-UUV8FP2\E.V.E. 08 <dariusz.majchrowicz@gmail.com>
2018-01-27 11:47:31 +0100

    temporary commit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,30 @@
-console.log('\'Yo \'Allo!');
+console.log('Script is loaded');
+
+document.onreadystatechange = () => {
+  if (document.readyState === 'complete') {
+    console.log('%c Document is ready', 'color: #42dcf4');
+    let wheel = new Wheel();
+    wheel.init();
+  }
+};
+
+// ======= Wheel.JS =======
+
+class Wheel {
+  constructor() {
+    this.mainCircle = document.querySelector('.main-circle');
+    this.outerCircle = document.querySelector('.outer-circle');
+  }
+  addEventListeners() {
+    this.mainCircle.addEventListeners('mousedown', () => {
+      console.log('%c Whell is ready', 'color: #42dcf4');
+      this.outerCircleRotate();
+    });
+  }
+  outerCircleRotate() {
+    TweenMax.to(this.outerCircle, 3, { rotation: '360deg' });
+  }
+  init() {
+    console.log('%c Whell is ready', 'color: #42dcf4');
+  }
+}
