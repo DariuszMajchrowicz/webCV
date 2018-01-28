834379fb92dfbdf992a1ea9d9af0c66e169850c1
DESKTOP-UUV8FP2\E.V.E. 08 <dariusz.majchrowicz@gmail.com>
2018-01-28 20:54:28 +0100

    finalny spin is spinning... kindof

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -12,19 +12,43 @@ document.onreadystatechange = () => {
 
 class Wheel {
   constructor() {
+    this.canSpin = true;
+    this.currentCircle = 0;
+    this.currentAngle = 0;
+    this.numberOfCircles = 6;
+    this.smallCirles = document.querySelectorAll('.wheel-circle');
+
     this.mainCircle = document.querySelector('.main-circle');
     this.outerCircle = document.querySelector('.outer-circle');
   }
   addEventListeners() {
-    this.mainCircle.addEventListeners('mousedown', () => {
-      console.log('%c Whell is ready', 'color: #42dcf4');
-      this.outerCircleRotate();
-    });
+    this.smallCirles[0].addEventListener('mousedown', () => { this.circleRotate(0); });
+    this.smallCirles[1].addEventListener('mousedown', () => { this.circleRotate(1); });
+    this.smallCirles[2].addEventListener('mousedown', () => { this.circleRotate(2); });
+    this.smallCirles[3].addEventListener('mousedown', () => { this.circleRotate(3); });
+    this.smallCirles[4].addEventListener('mousedown', () => { this.circleRotate(4); });
+    this.smallCirles[5].addEventListener('mousedown', () => { this.circleRotate(5); });
   }
-  outerCircleRotate() {
-    TweenMax.to(this.outerCircle, 3, { rotation: '360deg' });
+  circleRotate(_circleNumber) {
+    if ( !this.canSpin ) { return; }
+    this.canSpin = false;
+
+    this.currentAngle = 360 * _circleNumber / this.numberOfCircles;
+    if ( this.currentCircle >= _circleNumber ) {
+      TweenMax.to(this.outerCircle, 1, { rotation: this.currentAngle + 360, onComplete: () =>{
+        TweenMax.set(this.outerCircle, { rotation: this.currentAngle })
+        this.canSpin = true;
+      }});
+    }
+    else{
+      TweenMax.to(this.outerCircle, 1, { rotation: this.currentAngle, onComplete: ()=>{
+        this.canSpin = true;
+      }})
+    }
+    
+    this.currentCircle = _circleNumber;
   }
   init() {
-    console.log('%c Whell is ready', 'color: #42dcf4');
+    this.addEventListeners();
   }
 }
